57cb534198fd8d0a46c99fd7ddc69c964c0e1166
omurilolima <muriloserafimlima@gmail.com>
2024-05-21 20:06:55 +0100

    Create alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-    </div>
+    <>
+      <div>
+        <Alert>
+          Hello <span>World</span>
+        </Alert>
+      </div>
+    </>
   );
 }
 
