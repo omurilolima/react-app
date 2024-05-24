3569622e7bfa583c0b21f08000ef2d991a44e277
omurilolima <muriloserafimlima@gmail.com>
2024-05-24 18:30:47 +0100

    Use Immer to update an Array of Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Like from "./components/Like";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
@@ -7,11 +7,23 @@ function App() {
     { id: 2, tittle: "Bug 2", fixed: false },
   ]);
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
     <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.tittle} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
