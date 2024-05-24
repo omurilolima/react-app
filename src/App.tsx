0980b871e55bdcefbb85ef7c0ba79d8fc32efb05
omurilolima <muriloserafimlima@gmail.com>
2024-05-24 20:39:21 +0100

    Exercise - Building and ExpandableText Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import { useState } from "react";
+import ExpendableText from "./components/ExpendableText";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
-
-  return <div></div>;
+  return (
+    <div>
+      <ExpendableText maxChars={100}>
+        Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa
+        gentis num é. Leite de capivaris, leite de mula manquis sem cabeça. Pra
+        lá, depois divoltis porris, paradis. Em pé sem cair, deitado sem dormir,
+        sentado sem cochilar e fazendo pose. Paisis, filhis, espiritis santis.
+        Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e
+        fermentis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua
+        vidis. In elementis mé pra quem é amistosis quis leo.
+      </ExpendableText>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ExpendableText.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+interface Props {
+  children: string;
+  maxChars?: number;
+}
+
+const ExpendableText = ({ children, maxChars = 100 }: Props) => {
+  const [isExpanded, setExpanded] = useState(false);
+
+  if (children.length <= maxChars) return <p>{children}</p>;
+
+  const text = isExpanded ? children : children.substring(0, maxChars);
+
+  return (
+    <p>
+      {text}...{" "}
+      <button onClick={() => setExpanded(!isExpanded)}>
+        {isExpanded ? "Less" : "More"}
+      </button>
+    </p>
+  );
+};
+
+export default ExpendableText;
