3a175c4668f6ddbe7b11422080651e394950b155
omurilolima <muriloserafimlima@gmail.com>
2024-05-30 18:42:53 +0100

    Handling Errors with Axios

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,17 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []);
   return (
     <ul>
+      {error && <p className="text-danger">{error}</p>}
       {users.map((user) => (
         <li key={user.id}>{user.name}</li>
       ))}
